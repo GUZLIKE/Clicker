e10143aa8ffbdffc21eb923bdb8cdff3b16a8131
GUZLIKE <vinnickow.e@yandex.ru>
2023-01-14 12:10:58 +0300

    ADDED SAVES

--- app/src/main/java/ru/guzlik/berserkclicker/MainActivity.java
+++ app/src/main/java/ru/guzlik/berserkclicker/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -16,16 +17,18 @@ public class MainActivity extends AppCompatActivity {
     static public int count = 0;
     static public int plus_kill = 10;
     ImageView kill, improve;
+    SharedPreferences preferences;
 
     static public TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        SharedPreferences preferences;
         kill = (ImageView) findViewById(R.id.kill);
         improve = (ImageView) findViewById(R.id.improve);
         text = (TextView) findViewById(R.id.count);
+        load();
         Kill();
         Improve();
     }
@@ -36,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 count += plus_kill;
                 text.setText(count + "");
+                save();
             }
         });
     }
@@ -49,4 +53,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    void save(){
+        preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("clicks", count);
+        editor.commit();
+    }
+
+    void load(){
+        preferences = getPreferences(MODE_PRIVATE);
+        count = preferences.getInt("clicks", 0);
+        text.setText(count + "");
+    }
+
 }
